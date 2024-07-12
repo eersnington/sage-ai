cc03c02d2f64c1b73c7571ba988d44a41c332acd
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-13 01:27:16 +0400

    Update icons.tsx

--- components/ui/icons.tsx
+++ components/ui/icons.tsx
@@ -1,21 +1,18 @@
 'use client'
 
 import { cn } from '@/lib/utils'
+import Image from 'next/image'
 
-function IconLogo({ className, ...props }: React.ComponentProps<'svg'>) {
+function IconLogo({ className, ...props }: { className?: string }) {
   return (
-    <svg
-      fill="currentColor"
-      viewBox="0 0 256 256"
-      role="img"
-      xmlns="http://www.w3.org/2000/svg"
-      className={cn('h-4 w-4', className)}
+    <Image
+      src="/logo.png"
+      alt="sage-ai"
+      width={32}
+      height={32}
+      className={cn(className)}
       {...props}
-    >
-      <circle cx="128" cy="128" r="128" fill="black"></circle>
-      <circle cx="102" cy="128" r="18" fill="white"></circle>
-      <circle cx="154" cy="128" r="18" fill="white"></circle>
-    </svg>
+    />
   )
 }
 
