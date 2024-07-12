eb4b2082e506ac541c990a6c259ddb0200af95ce
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-12 21:34:01 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {};
+import { withContentlayer } from "next-contentlayer2"
 
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    turbo: {
+        resolveExtensions: [
+          '.mdx',
+          '.tsx',
+          '.ts',
+          '.jsx',
+          '.js',
+          '.mjs',
+          '.json',
+        ],
+      },
+    serverComponentsExternalPackages: ["@prisma/client"],
+  },
+}
+
+export default withContentlayer(nextConfig)
