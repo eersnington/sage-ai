3c287fe30179291e7b037ecfd2ddaf83775166c7
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-15 02:45:24 +0400

    Update chat.tsx

--- components/chat.tsx
+++ components/chat.tsx
@@ -2,9 +2,11 @@
 
 import { useEffect } from 'react'
 import { usePathname } from 'next/navigation'
+import { motion } from 'framer-motion'
 import { ChatPanel } from './chat-panel'
 import { ChatMessages } from './chat-messages'
 import { useUIState } from 'ai/rsc'
+import { TypewriterEffect } from './typewriter-effect'
 
 type ChatProps = {
   id?: string
@@ -21,8 +23,34 @@ export function Chat({ id, query }: ChatProps) {
     }
   }, [id, path, messages, query])
 
+  const words = [
+    { text: 'Market', className: '' },
+    { text: 'Insights', className: '' },
+    { text: 'in', className: '' },
+    { text: 'Minutes', className: 'text-green-500 dark:text-green-500' },
+  ]
+
   return (
     <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4">
+      {!path.includes('search') && ( // Conditionally render if not in /search/[slug]
+        <>
+          <motion.div
+            className="text-neutral-600 dark:text-neutral-200 text-base text-center mb-6"
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            Stop wasting time on market research
+          </motion.div>
+          <motion.div
+            initial={{ opacity: 0, y: -20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.3 }}
+          >
+            <TypewriterEffect words={words} />
+          </motion.div>
+        </>
+      )}
       <ChatMessages messages={messages} />
       <ChatPanel messages={messages} query={query} />
     </div>
