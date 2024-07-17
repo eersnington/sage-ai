1b97a1fe77cd6a44d3d60c70c9d4ae8c2d5e4172
Sree Narayanan <sreeaadhi07@gmail.com>
2024-07-17 07:22:05 +0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import { withContentlayer } from "next-contentlayer2"
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['api.producthunt.com'],
+  },
   experimental: {
     turbo: {
         resolveExtensions: [
